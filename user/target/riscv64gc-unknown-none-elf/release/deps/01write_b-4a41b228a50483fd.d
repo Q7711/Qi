5739e852c055c3af24fa1d15ae79014979a01fc5
Alkaid_77 <142142974+Q7711@users.noreply.github.com>
2023-12-14 23:41:41 +0800

    实验四
    实验四

---
+++ user/target/riscv64gc-unknown-none-elf/release/deps/01write_b-4a41b228a50483fd.d
@@ -0,0 +1,5 @@
+/mnt/user/target/riscv64gc-unknown-none-elf/release/deps/01write_b-4a41b228a50483fd: src/bin/01write_b.rs
+
+/mnt/user/target/riscv64gc-unknown-none-elf/release/deps/01write_b-4a41b228a50483fd.d: src/bin/01write_b.rs
+
+src/bin/01write_b.rs:
