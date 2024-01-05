df221c7e0ff153fd9c0615daa74e2b6dd833b06b
Alkaid_77 <142142974+Q7711@users.noreply.github.com>
2024-01-05 22:52:13 +0800

    experiment07
    实验七

---
+++ user/target/riscv64gc-unknown-none-elf/release/deps/00power_3-94a403749ab241bc.d
@@ -0,0 +1,5 @@
+/mnt/user/target/riscv64gc-unknown-none-elf/release/deps/00power_3-94a403749ab241bc: src/bin/00power_3.rs
+
+/mnt/user/target/riscv64gc-unknown-none-elf/release/deps/00power_3-94a403749ab241bc.d: src/bin/00power_3.rs
+
+src/bin/00power_3.rs:
